596ecacfdb2b04c4bcda473ce73d1ef5ed298b92
S7pt <chaos160702@gmail.com>
2020-10-24 01:07:42 +0300

    Added 3rd lab

---
+++ scripts/main-script.js
@@ -0,0 +1,65 @@
+import {
+    Insect,
+    AddInsect
+} from './model.js'
+
+const elementAdder = document.getElementById('add-button');
+const count = document.getElementById('count-button');
+const sort = document.getElementById('sort-button');
+const sortSlider = document.querySelector(".slider");
+const contentBox = document.getElementById('content');
+const search = document.getElementById('search');
+let isOn = false;
+let counter = 1;
+let insects = [];
+
+elementAdder.addEventListener('click', (event) => {
+    let id = counter;
+    counter += 1;
+    let name = Math.random().toString(36).substring(7) + " Caterpillar";
+    let description = "The cute, grass-eating creature wants to be your friend";
+    let price = Math.floor(Math.random() * 100);
+    let insect = new Insect(id, name, description, price);
+    insects.push(insect);
+    AddInsect({ id, name, price, description });
+    updateList(insects);
+    document.getElementById("search-bar").value = '';
+})
+
+count.addEventListener('click', (event) => {
+    let totalPrice = 0;
+    for (let i = 0; i < insects.length; i++) {
+        totalPrice += insects[i].price;
+    }
+    document.getElementById('net-worth-price').innerText = totalPrice + '$';
+})
+
+sort.addEventListener('click', (event) => {
+    sortSlider.classList.toggle("active");
+    if (isOn) {
+        updateList(insects)
+        isOn=false;
+        return;
+    }
+    let sortList = [...insects];
+    sortList.sort((a, b) => a.price - b.price);
+    updateList(sortList);
+    isOn=true;
+})
+
+function updateList(givenList) {
+    contentBox.innerHTML = '';
+    for (let i = 0; i < givenList.length; i++) {
+        let id = givenList[i].id;
+        let name = givenList[i].name;
+        let description = givenList[i].description;
+        let price = givenList[i].price;
+        AddInsect({ id, name, description, price, })
+    }
+}
+search.addEventListener('click', (event) => {
+    let text = document.getElementById("search-bar").value;
+    let regEx = new RegExp(text, 'i');
+    let searchResult = insects.filter(insect => regEx.test(insect.name) || regEx.test(insect.description));
+    updateList(searchResult)
+});
---
+++ scripts/model.js
@@ -0,0 +1,26 @@
+export class Insect {
+    constructor(id, name, description, price) {
+        this.id = id;
+        this.description = description;
+        this.name = name;
+        this.price = price;
+    }
+}
+export const InsectTemplate = ( id, name, price, description ) => 
+    `<div class="template-div">
+    <div class="template-image"></div>
+    <h3 class="template-title">${id}.${name}</h3>
+    <p class="template-text">${description}</p>
+    <div class="template-price">
+        <p class="template-price-text">Price: </p>
+        <p class="template-price-number"> ${price}$</p>
+    </div>
+<div class="aside__buttons">
+    <button id="edit-button" class="edit-button">Edit</button>
+    <button class="remove-button" data-id="${id}">Remove</button>
+</div>
+</div>`;
+
+export const AddInsect=({id,name,price,description})=>{
+document.getElementById("content").insertAdjacentHTML('beforeend', InsectTemplate(id,name,price,description))
+}
